169009083609147285bc33b7e18aba06548209d5
Salim66 <salimhasanriad8@gmail.com>
2022-12-29 19:57:46 +0600

    Product crud dynamic

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,8 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue';
+import {createApp} from 'vue'
+import store from './store'
+import router from './router'
+import './style.css';
 import './index.css';
-import store from './store';
-import router from './router';
+import currencyUSD from './filters/currency.js'
 
-createApp(App).use(store).use(router).mount('#app')
+import App from './App.vue'
+
+const app = createApp(App);
+
+app
+  .use(store)
+  .use(router)
+  .mount('#app')
+;
+
+app.config.globalProperties.$filters = {
+  currencyUSD
+}
